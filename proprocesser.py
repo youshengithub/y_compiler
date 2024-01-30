3f7a7ec3f54a1523cd657e4edb9eff3d8b71119d
Youshen <1391419813@qq.com>
2024-01-30 16:10:15 +0800

    完成预处理器定义

--- proprocesser.py
+++ proprocesser.py
@@ -17,10 +17,34 @@ class preprocesser:
    
     def process_include(self,text):
         lines=text.split("\n")
-
-        return text
+        code=""
+        for line in lines:
+            if(line.startswith("#include")):
+                file_path=line[8:-1]
+                try:
+                    with open(file_path, 'r', encoding='utf-8') as file:
+                        content = file.read()
+                except FileNotFoundError:
+                    print("错误：文件未找到。请检查文件路径是否正确。")
+                except IOError:
+                    print("错误：无法读取文件。")
+                code+=self.process_include(content)
+            else:
+                code+=line+"\n"
+        return code
     def process_define(self,text):
-        return text
+        tags={}
+        lines=text.split("\n")
+        code=""
+        for line in lines:
+            if(line.startswith("#define")):
+                defines=line[7].split(" ")
+                tags[defines[0]]=defines[1]
+            else:
+                for k,v in tags.items():
+                    line=line.replace(k,v)
+                code+=line+"\n"
+        return code
     def process_space(self,text):
         return text.replace('\n', '').replace(' ', '').replace('\t', '')
     def process(self,text):#处理预处理器命令#
