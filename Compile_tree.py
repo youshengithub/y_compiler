5fa52c38b4c454ed3285bbb1f11225e2d4862302
Youshen <1391419813@qq.com>
2024-05-02 21:50:32 +0800

    独立语法分析和编译版本

---
+++ Compile_tree.py
@@ -0,0 +1,380 @@
+import re,itertools,copy
+from token_ana import *
+def Complie(name,rule,oplist,codelist,area_tree):
+    code=""
+    #return code
+    if(name=="VAR"): 
+        if(oplist[0]!="EAX"):#
+            var=y_token.trans_var(oplist[0])
+            base=0
+            #ESP的位置用于存储temp变量
+            for i in var:
+                find_var=area_tree.find_token(i[0]) #看一看是啥子类型
+                base=find_var.start_pos
+                if(find_var==None):
+                    print("变量未定义",oplist[0],"--->",i[0])
+                    assert(1==0)
+                if(len(i[1:])!=len(find_var.muti_dimension)):
+                    print("维度不匹配 ",oplist[0],"--->",i[0]," 变量原始维度:",len(find_var.muti_dimension),"变量引用维度:",len(i)-1)
+                    assert(1==0)
+                code+="MOV EAX "+str(base)+"\n"#载入0号位置
+                accumulate_demension=[]
+                current=1
+                for demension in reversed(find_var.muti_dimension):
+                    accumulate_demension.append(current)
+                    current*=demension
+                accumulate_demension=list(reversed(accumulate_demension))
+                for index in range(len(find_var.muti_dimension)) :
+                    #需要判断是j是数字还是变量
+                    if(i[index+1].isdigit()):
+                        if(int(i[index+1])>find_var.muti_dimension[index]): #算了 不想去判断:
+                            print("维度超过限制 ",oplist[0],"--->",i[0],"的第",index,"维,变量原始维度:",find_var.muti_dimension[i],"变量引用维度:",int(i[index+1]))
+                            assert(1==0)
+                        code+="MOV EBX "+i[index+1]+"\n"
+                    else:
+                        find_var=area_tree.find_token(i[index+1]) 
+                        if(find_var==None):
+                            print("变量未定义",oplist[0],"--->",i[index+1])
+                            assert(1==0)
+                        code+="MOV EBX "+str(find_var.start_pos)+"\n"
+                    code+="MUL EBX "+str(accumulate_demension[index])+"\n"
+                    code+="ADD EAX EBX\n"
+                        #寻找到变量位置                 
+                #最后 EAX就是变量的位置！
+                oplist.clear()
+                oplist.append("EAX")   
+    elif(name=="TOKEN"): #注意到有些token是
+        #看一看到底是哪一个token  
+        pass
+    elif(name=="CONST"):
+        pass
+    elif(name=="AREA"):
+        for i in codelist:code+=i
+    elif(name=="REGS"):
+        pass
+    elif(name=="DIM"): #这里要产生巨变！
+        type=oplist[0]
+        #解析一下a[10][10]这种类型的
+        var=y_token.trans_token(oplist[1])
+        num=1
+        for i in var[1:]: num*=int(i)
+        find_type=area_tree.find_token(type)
+        # find_var=area_tree.find_token(var[0])
+        # # if(find_var!=None):
+        # #     print("重定义符号")
+        # #     assert(1==0)
+        if(find_type==None):
+            print("编译出错,类型未定义")
+            assert("1==0")
+        if(rule=="$TYPE$->$TOKEN$"): #进行解析
+            
+            start_pos=area_tree.clac_current_pos()
+            t=y_token()
+            t.set_as_variable(var[0],find_type.size*num,type,start_pos,[int(i) for i in var[1:]])
+            area_tree.append_var(t,t.size) #只有变量才会分配区域大小
+            code="ALLOC "+str(t.size)+"\n" #
+        elif(rule=="$TYPE$->$TOKEN$=$STRING$"):
+            pass #oplist[0]是var oplist[1]是strig
+            assert(type=="int" or type=="double")
+            string=oplist[2]
+            length=num
+            if(length<len(string)):
+                length=len(string)
+            code="ALLOC "+str(length)+"\n"  
+            base=str(area_tree.clac_current_pos()) 
+            t=y_token()
+            t.set_as_variable(var[0],find_type.size*num,type,base,[int(i) for i in var[1:]])
+            area_tree.append_var(t)
+            for i in range(len(string)):
+                code+="MOV "+base+":"+str(i)+" "+str(ord(string[i]))+ "\n"
+            code+="MOV "+base+":"+str(len(string))+ " 0\n"
+        else:
+            print(oplist)
+    elif(name=="OP"):
+        for i in codelist: code+=i 
+        pass
+    elif(name=="ADD"):
+        if(rule=="$OPN$+$OPN$"):
+            code="MOV EAX "+str(oplist[0])+"\n"
+            code+="ADD EAX "+str(oplist[1])+"\n"
+        elif(rule=="$OPN$+$OP$"):
+            code=codelist[0]
+            code+="ADD EAX "+str(oplist[0])+"\n"
+        elif(rule=="$OP$+$OPN$"):
+            code=codelist[0]
+            code+="ADD EAX "+str(oplist[-1])+"\n"
+        elif(rule=="$OP$+$OP$"):
+            code=codelist[1]
+            code+="MOV EBX EAX\n"
+            code+=codelist[0]
+            code+="ADD EAX EBX\n"
+        else:
+            assert(1==0)
+    elif(name=="SUB"):
+        if(rule=="$OPN$-$OPN$"):
+            code="MOV EAX "+str(oplist[0])+"\n"
+            code+="SUB EAX "+str(oplist[1])+"\n"
+        elif(rule=="$OPN$-$OP$"):
+            code=codelist[0]
+            code+="MOV EBX "+str(oplist[0])+"\n"
+            code+="SUB EBX EAX\n"
+            code+="MOV EAX EBX\n"
+        elif(rule=="$OP$-$OPN$"):
+            code=codelist[0]
+            code+="SUB EAX "+str(oplist[-1])+"\n"
+        elif(rule=="$OP$-$OP$"):
+            code=codelist[1]
+            code+="MOV EBX EAX\n"
+            code+=codelist[0]
+            code+="SUB EAX EBX\n"
+        else:
+            assert(1==0)
+    elif(name=="MUL"): #注意到可能会被修正成代码！ op可能会被修正成位置
+        if(rule=="$OPN$*$OPN$"):
+            code="MOV EAX "+str(oplist[0])+"\n"
+            code+="MUL EAX "+str(oplist[1])+"\n"
+        elif(rule=="$OPN$*$OP$"):
+            code=codelist[0]
+            code+="MUL EAX "+str(oplist[0])+"\n"
+        elif(rule=="$OP$*$OPN$"):
+            code=codelist[0]
+            code+="MUL EAX "+str(oplist[-1])+"\n"
+        elif(rule=="$OP$*$OP$"):
+            code=codelist[1]
+            code+="MOV EBX EAX\n"
+            code+=codelist[0]
+            code+="MUL EAX EBX\n"
+        else:
+            assert(1==0)
+    elif(name=="DIV"):
+        if(rule=="$OPN$/$OPN$"):
+            code="MOV EAX "+str(oplist[0])+"\n"
+            code+="DIV EAX "+str(oplist[1])+"\n"
+        elif(rule=="$OPN$/$OP$"):
+            code=codelist[0]
+            code+="MOV EBX "+str(oplist[0])+"\n"
+            code+="DIV EBX EAX\n"
+            code+="MOV EAX EBX\n"
+        elif(rule=="$OP$/$OPN$"):
+            code=codelist[0]
+            code+="DIV EAX "+str(oplist[-1])+"\n"
+        elif(rule=="$OP$/$OP$"):
+            code=codelist[1]
+            code+="MOV EBX EAX\n"
+            code+=codelist[0]
+            code+="DIV EAX EBX\n"
+        else:
+            assert(1==0)
+    elif(name=="AND" or name=="XOR" or name=="OR" or name=="MOD"):
+        if(bool(re.match("\\$OPN\\$.+\\$OPN\\$", rule))):
+            code="MOV EAX "+str(oplist[0])+"\n"
+            code+=name+" EAX "+str(oplist[1])+"\n"
+        elif(bool(re.match("\\$OPN\\$.+\\$OP\\$", rule))):
+            code=codelist[0]
+            code+=name+" EAX "+str(oplist[0])+"\n"
+        elif(bool(re.match("\\$OP\\$.+\\$OPN\\$", rule))):
+            code=codelist[0]
+            code+=name+" EAX "+str(oplist[-1])+"\n"
+        elif(bool(re.match("\\$OP\\$.+\\$OP\\$", rule))):
+            code=codelist[0]
+            code+="MOV EBX EAX\n"
+            code+=codelist[1]
+            code+=name+" EAX EBX\n"
+        else:
+            assert(1==0)
+
+    elif(name=="NOT"): #处理单目运算符
+        if(rule=="!$OPN$"):
+            code="NOT EAX "+str(oplist[0])+"\n"
+        elif(rule=="!$OP$"):
+            code=codelist[0]
+            code+="MOV EBX EAX\n"
+            code+="NOT EAX EBX\n"
+        else:
+            pass
+        pass
+    elif(name=="GETP"):
+        code+="LEA EAX "+str(oplist[0])+"\n"
+        pass
+    elif(name=="SETP"):
+        if(rule=="*$OPN$"): #在此处对oplist进行修正！ or 插入指令  #总是会把计算压入stack
+            code+="PUSH "+str(oplist[0])+"\n"
+            #oplist=[]
+            #code="SEA  EAX" +str(oplist[0])+"\n"
+            pass
+        elif(rule=="*$OP$"):#eax是需要修正的结果 不支持 EBX也可能被人用掉了！ 我需要一个不会被人用掉的寄存器或者位置！
+            for i in codelist: code+=i #PUSH EBX POP
+            code+="PUSH EAX\n"
+            pass
+        else:
+            pass
+        pass
+    elif(name=="EQUAL"):
+        #可能会有code针对左边的地方进行计算！
+        if(rule=="$VAR$=$OPN$"):
+            code="MOV "+str(oplist[0]) +" "+str(oplist[1])+"\n"
+        elif(rule=="$VAR$=$OP$"):   
+            code=codelist[0]
+            code+= "MOV "+str(oplist[0])  + " EAX\n"
+        elif(rule=="$SETP$=$OPN$"):  #这里可能会有点问题 
+            for i in codelist: code+=i 
+            code+="POP EBX\n"
+            code+="SEA EBX "+str(oplist[-1])+"\n" #这里的oplist不一定多少个数据
+            pass
+        elif(rule=="$SETP$=$OP$"):
+            for i in codelist: code+=i 
+            code+="POP EBX\n"
+            code+="SEA EBX EAX\n"
+            pass
+        else:
+            pass
+        pass
+    elif(name=="SENTENCE"):
+        for i in codelist: code+=i 
+        pass
+    elif(name=="JUDGE"):
+        if(rule.find("<=")!=-1):
+            pass
+        elif(rule.find(">=")!=-1):
+            pass
+        if(rule.find("<")!=-1):
+            code="LESS "+oplist[0]+" "+oplist[1]+"\n"
+        elif(rule.find(">")!=-1):
+            code="GREATER "+oplist[0]+" "+oplist[1]+"\n"
+        elif(rule.find("==")!=-1):
+            code="EQUAL "+oplist[0]+" "+oplist[1]+"\n"
+        elif(rule.find("!=")!=-1):
+            code="EQUAL "+oplist[0]+" "+oplist[1]+"\n"
+            code+="RF\n"
+        elif(rule.find("&&")!=-1):
+            code=codelist[0]
+            code+="JPIF "+str((codelist[1]).count("\n")+1)+"\n"
+            code+=codelist[1]
+        elif(rule.find("||")!=-1):
+            code=codelist[0] #如果不是这样的就跳转到末尾
+            code+="JPNIF "+str((codelist[1]).count("\n")+1)+"\n"
+            code+=codelist[1]
+        else:
+            for i in codelist: code+=i 
+    elif(name=="IF"):
+        code=codelist[0]
+        code+="JPIF "+str((codelist[1]).count("\n")+2)+"\n"
+        code+=codelist[1]
+        code+="JMP "+str((codelist[2]).count("\n")+1)+"\n"
+        code+=codelist[2]
+        pass
+    elif(name=="DO"):
+        code=codelist[0]+codelist[1]
+        code+="RF\n"
+        code+="JPIF -"+str((codelist[0]+codelist[1]).count("\n")+1)+"\n"
+        pass
+    elif(name=="WHILE"):
+        code=codelist[0]
+        code+="JPIF "+str(codelist[1].count("\n")+2)+"\n"
+        code+=codelist[1]
+        code+="JMP -"+str((codelist[0]+codelist[1]).count("\n")+1)+"\n"
+        pass
+    elif(name=="FOR"):
+        #需要看一看有没有3
+        code=codelist[0]+codelist[1]
+        code+="JPIF "+str((codelist[3]+codelist[2]).count("\n")+2)+"\n"
+        code+=codelist[3]+codelist[2]
+        code+="JMP -"+str((codelist[1]+codelist[3]+codelist[2]).count("\n")+1)+"\n"
+        
+        pass
+    elif(name=="PRINT"):
+        code="OUT "+str(oplist[0])+"\n"
+        pass
+    elif(name=="IN"):
+        code="IN EAX\n"
+    elif(name=="FUNCNAME"): #funcname/structure_name
+        sub_area=area_tree.new_area(True,oplist[0])
+        area_tree=sub_area#创建顶级域 在AREA的时候恢复顶级域
+        pass#在这里就要创建新的顶级域了
+    elif(name=="FUNC"):#在定义的时候，不要执行语句
+        for i in codelist: code+=i #注意到这里已经完成了赋值 这里面分了三段
+        revise_code=code.split("\n")[:-1]
+        code=""
+        for i in range(len(revise_code)):
+            toend=len(revise_code)-i
+            revise_code[i]=revise_code[i].replace("END",str(toend))
+            code=code+revise_code[i]+"\n"
+        #需要把code里面的END全部替换掉！
+        code+="MOV ESP $0:-6\n"
+        code+="MOV EBX $0:-3\n"
+        code+="MOV EFG $0:-2\n"
+        code+="MOV ETP $0:-1\n" #不用再跳转了！这里已经写好了参数了！ 注意到这里有一个坑按道理来说必须同时还原,
+        code+="MOV EBP $0:-5\n" #使用临时寄存器ETP暂时保存该跳转的结果。
+        code+="MOV EIP ETP\n"
+        code="JMP "+str(code.count("\n")+1)+"\n"+code
+        code="ALLOC @"+oplist[1]+"\n"+code
+        area_tree=area_tree.father
+        #code=codelist[0] #还没处理return问题嘞
+        pass
+    elif(name=="CALL"):#call 然后eax传入参数！
+        #oplist[1] 就是标签名 base+最高， 然后把参数都move过去 然后base-最高，就ok了！
+        #保存ESP和EBP
+        #这里不能这样计算 EAX无用了 因为不会被恢复
+        code="MOV EAX ESP\n"
+        code+="SUB EAX EBP\n"
+        code+="MOV $0:EAX ESP\n"
+        code+="MOV $1:EAX EBP\n"
+        code+="MOV $2:EAX EAX\n"
+        code+="MOV $3:EAX EBX\n"
+        code+="MOV $4:EAX EFG\n"
+        code+="MOV EBX EAX\n" #保存一下我要压ip的时候用
+        
+        code+="ADD ESP 6\n"
+        code+="MOV ETP ESP\n"
+        code+=codelist[0] #压入参数 EAX EBX 可用 EBP ETP 可用否？
+        
+        code+="MOV EAX EIP\n"
+        code+="ADD EAX 4\n"
+        code+="MOV $5:EBX EAX\n"#最后才来压入eip! 不行 这里的EAX的值g了
+        
+        code+="MOV EBP ETP\n" #压完了参数再给ebp复制
+        code+="JMP @"+oplist[0]+"\n" #这里需要绝对地址！
+        #然后需要执行跳转！
+        pass
+    elif(name=="AREA"):
+        #在这里分配作用域
+        pass
+    elif(name=="PAR"):#这里是形参
+        #code="SUB EBP 1\n" #不太需要动EBP
+        pass
+    elif(name=="ARG"): #这里是call的实参  只能使用EAX了 但是我又需要计算op op的值必须要存在eax里面
+        #可以使用EAX 但是在算数的时候可能会使用任意参数啊！ 不要把EBX算数寄存器拿来用作控制！
+        code="MOV EAX ESP\n"
+        code+="SUB EAX EBP\n"
+        if(rule=="$OPN$"):
+            code+="MOV $0:EAX "+oplist[0]+"\n"
+            
+        elif(rule=="$OP$"):
+            print("NOT_SUPPORT")
+            assert(1==0)
+            code+="MOV $0:EAX "+oplist[0]+"\n"
+        else:
+            pass
+        code+="ADD ESP 1\n"   
+        pass
+    elif(name=="RETURN"):
+        if("$OP$" in rule):
+            for i in codelist: code+=i #JMP到最后才行！ 这里怎么写最后啊！
+        elif("$OPN$" in rule):
+            code="MOV EAX "+oplist[0]+"\n"
+        else:
+            pass 
+        code+="JMP END\n"# 等到func来填充这个就可以了哦
+        pass
+    elif(name=="TYPE"):
+        pass
+    elif(name=="STRUCTURE"):
+        area_tree=area_tree.father
+        t=y_token()
+        t.set_as_structure(oplist[0],1,[],[])
+        area_tree.append_var(t)
+        
+    elif(name=="ASM"):
+        code+=oplist[0].replace("\\n","\n")
+        pass
+    return code,area_tree
---
+++ Construct_tree.py
@@ -0,0 +1,215 @@
+import re,itertools,copy
+from runner import Runner
+# 打开文本文件
+from token_ana import *
+from postprocesser import Postprocesser
+from preprocesser import Preprocesser
+import Compile_tree
+class Compoment:
+    Cs={}   #语句类型
+    unmatch={}
+    def __init__(self, name_,config_,repeat_,no_start,is_keywords):
+        self.configs=config_
+        self.name=name_
+        self.repeat=repeat_
+        self.no_start=no_start
+        self.is_keywords=is_keywords
+    def handelP(self,rule,text): 
+        next_index = rule[1:].find('$')
+        if(next_index==-1): return False
+        name=rule[1:next_index+1]
+        return Compoment.Cs[name].Rrcognize(text)
+    def HandleR(self,rule,text): 
+        text_c=text
+        oplist=[]
+        codelist=[]
+        while(rule!=""):
+            if(rule[0]=="@"): #这里很麻烦的啦！ 
+                next_index = rule[1:].find('@')
+                if(next_index==-1): 
+                    return False,text_c,oplist,codelist
+                pattern=rule[1:next_index+1] 
+                match = re.search(pattern, text)
+                if match!=None :
+                    match_text=text[:match.regs[0][1]] #仿佛这是最简单的办法
+                    text=text[match.regs[0][1]:]  
+                    rule=rule[2+len(pattern):]  
+                    if(self.name=="VAR"): #需要进行判断是什么词性
+                        if match_text in ["struct","class","void","in","asm","if","do","while","for","out","else","func","return","struct","int","double","continue"] :
+                            return False,text_c,oplist,codelist
+                        oplist.append(match_text)
+                    elif(self.name=="CONST"):
+                        oplist.append(match.group(0))
+                    elif(self.name=="STRING"): #需要有一个table 
+                        oplist.append(match.group(0)[1:-1])
+                    elif(self.name=="REGS"):
+                        oplist.append(match.group(0)) #直接把名称放进去
+                    elif(self.name=="TYPE"):
+                        oplist.append(match.group(0)) #直接把类型放进去
+                    elif(self.name=="TOKEN"):
+                        if match_text in ["struct","class","void","in","asm","if","do","while","for","out","else","func","return","struct","continue"] :
+                            return False,text_c,oplist,codelist
+                        oplist.append(match_text) #直接把token放进去
+                else:
+                    #if(text_c!=text): print("正则表达式无法识别---",rule,"-->\n",text)
+                    return False,text_c,oplist,codelist
+            elif(rule.startswith("$")):
+                succ,text,code,r_oplist=self.handelP(rule,text) #消除掉rule部分 并且这里的code 没有用上！
+                if(succ==True):
+                    dollar_index = rule[1:].find('$')
+                    rule=rule[2+dollar_index:] 
+                    oplist+=r_oplist
+                    if(code!=""):
+                        codelist+=[code]
+                    continue
+                else:
+                    #if(text_c!=text):     print("$$识别错误---",rule,"-->\n",text,"\n")
+                    return False,text_c,oplist,codelist
+            else: #这里用来消除关键字
+                    dollar_index = rule.find('$')
+                    keyword=rule[:dollar_index] if dollar_index != -1 else rule
+                    if(text.startswith(keyword)):
+                        text=text[len(keyword):]  
+                        rule=rule[len(keyword):]  
+                    else:
+                        #if(text_c!=text): print("有多余关键字未识别---",rule,"-->\n",text)
+                        return False,text_c,oplist,codelist
+        #print("识别成功:",self.name,"->",text_c,"--->",c_rule)
+        return True,text,oplist,codelist
+    def Rrcognize(self,text):
+        flag=False
+        repeat=True
+        r_oplist=[]
+        r_code=[]
+        while(repeat): #规则必须也是寻找最有可能的匹配
+            repeat=False
+            for rule in self.configs:#通过语句构建,这个选择一种规则！
+                textc=text
+                key=(hash(text),self.name,rule) #使用hash节约内存
+                if(key in Compoment.unmatch ): #使用缓存，避免重复解析
+                    if(Compoment.unmatch[key]=="PROCESSING"): #陷入重入
+                        succ=False
+                    else:
+                        succ,text,oplist,code_list=Compoment.unmatch[key]
+                else:
+                    Compoment.unmatch[key]="PROCESSING"
+                    succ,text,oplist,code_list=self.HandleR(rule,textc) #这里面没有传入代码 
+                    Compoment.unmatch[key]=(succ,text,oplist,code_list,)
+                if(succ): 
+                    flag=True
+                    repeat=self.repeat
+                    r_oplist=oplist
+                    compile_txt=textc[0:len(textc)-len(text)]
+                    compiled_code=[(self.name,rule,oplist,code_list,compile_txt )]
+                    r_code+=compiled_code #+" Rule: "+rule
+                    break   #这里仿佛也不应该call;最好是用 
+        return flag,text,r_code,r_oplist
+class Compiler:
+    #VARPos=环境
+    def __init__(self) -> None:
+        self.error=False
+    def ana2(self,text,codes=[]):
+        state=[(text,codes)]
+        ans_code=[]
+        while len(state)!=0:
+            (n_text,n_codes)=state[0]
+            print(f"解析进度:{100-100*len(n_text)/len(text):.2f}%")
+            state=state[1:]
+            ans_code=n_codes
+            if(n_text==""):
+                break
+            single_succ=""
+            for name,sentence in Compoment.Cs.items():
+                if(sentence.no_start==True): continue
+                succ,textc,code,r_oplist=sentence.Rrcognize(n_text)
+                if(succ==True):
+                    state.append((textc,n_codes+code))
+                    single_succ=name
+                    break
+            if(single_succ==""):
+                print("编译发生错误,当前编译位置",n_text if len(n_text)<50 else n_text[:50]+"...")
+        return ans_code
+
+    def revise_config(self,original_str):
+        pattern = r"<(.*?)>"
+        matches = re.findall(pattern, original_str)
+        split_parts = [match.split('|') for match in matches]
+        combinations = list(itertools.product(*split_parts))
+        ans=[]
+        for replace_tuple in combinations:
+            replaced_str = original_str
+            for item in replace_tuple:
+                replaced_str = re.sub(r"<.*?>", item, replaced_str, 1)  # 只替换第一个匹配的项
+            ans.append(replaced_str)
+        return ans
+    def construct_componets(self,file_path): #仿佛可以对规则进行修改<$$|;>将进行排列组合
+        with open(file_path, 'r') as file: #构造词类
+            for line in file:
+                line=line.replace(' ', '')
+                temp=line.split(":")
+                name=temp[0]
+                configs=temp[1].split("#")
+                if(configs[-1]==""):
+                    configs=configs[0:-1]
+                attribute=temp[2]
+                revised_configs=[]
+                for config in configs:
+                    revised_configs+=self.revise_config(config)
+                print(name,revised_configs)
+                Compoment.Cs[name]=Compoment (name,revised_configs,"REPEAT" in attribute, "NO_START" in attribute,"IS_KEYWORDS" in attribute)  
+    def Complie_file(self,text):
+        self.error=False
+        return self.real_compile(self.ana2(text))
+    def cut_str(self,i):
+        if(len(i)<50): return i
+        else: return i[0:50]+"..."
+    def show(self,node,code,prefix=""):
+        codelists=[]
+        if(isinstance(node,tuple)):
+            name=node[0]
+            rule=node[1]
+            oplist=node[2]
+            code_list=node[3]
+            source_text=node[4]
+            print(prefix+self.cut_str(name)+"-->"+self.cut_str(rule)+"-->"+self.cut_str(source_text))
+            if(code_list!=[]):
+                for i in code_list:
+                    for j in i:
+                        b_code=self.show(j,"",prefix+" ")
+                        codelists.append(b_code)
+                code_list=codelists
+            b_code,self.area_tree=Compile_tree.Complie(name,rule,oplist,code_list,self.area_tree)
+            code+=b_code
+            return code
+        else:   assert(1==0)
+    def real_compile(self,c_lists):
+        area_tree=varea(None,True,"Main")
+        t=y_token(type=token_type.structure)
+        t.name="int"
+        t.size=1
+        area_tree.append_var(copy.deepcopy(t))
+        t.name="double"
+        t.size=1
+        area_tree.append_var(t)
+        code=""
+        self.area_tree=area_tree
+        for i in c_lists:
+            b_code=self.show(i,"")
+            code+=b_code
+        print(area_tree)
+        return code
+if __name__=="__main__":
+    a=Compiler()
+    b=Runner()
+    c=Postprocesser()
+    d=Preprocesser()
+    a.construct_componets("Config.txt")
+    with open("code.txt", 'r', encoding='utf-8') as file:
+        content = file.read()
+    preprocessed_code=d.process(content)
+    code=a.Complie_file(preprocessed_code)
+    print(code)
+    # code=c.process(code)
+    # with open("IR.txt", 'w', encoding='utf-8') as file:
+    #     file.write(code)
+    #b.Run_from_code(code.split("\n"))
